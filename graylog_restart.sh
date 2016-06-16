b2739edf8866e91c490068c279582e008953e73e
Luca Pisani <luca.pisani@abstract.it>
2016-06-16 14:09:02 +0200

    refs #73414 graylog updated to 2.0.2

--- graylog_restart.sh
+++ graylog_restart.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
-LOGFILE=/opt/graylog2-server/log/graylog_restarted.log
-
 LOGS=$(awk -v d1="$(date -u --date="-5 min" "+%Y-%m-%d %H:%M:%S.000")" \
        -v d2="$(date -u "+%Y-%m-%d %H:%M:%S.%3N")" \
-       '$0 > d1 && $0 < d2 || $0 ~ d2' /opt/graylog2-server/log/graylog-server.log \
+       '$0 > d1 && $0 < d2 || $0 ~ d2' /opt/graylog2/log/graylog-server.log \
        | grep "ERROR: org.graylog2.alarmcallbacks.EmailAlarmCallback - Stream" )
 
 if [[ $LOGS ]]; then
    kill -9 `cat /tmp/graylog.pid`
-   /opt/graylog2-server/bin/graylogctl start
+   /opt/graylog2/bin/graylogctl start
    echo "$(date -u "+%Y-%m-%d %H:%M:%S.%3N") Graylog server restarted"  >> $LOGFILE
 fi
